7bbc9deb76a4f8e8fb6db1e86c2064d5a1b875d0
BlackFox <3296895445@qq.com>
2016-07-26 19:06:35 +0800

    下载好友群组列表任务类的创建

---
+++ src/cn/ucai/superwechat/task/DownloadGroupListTask.java
@@ -0,0 +1,57 @@
+package cn.ucai.superwechat.task;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Map;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.bean.GroupAvatar;
+import cn.ucai.superwechat.bean.Result;
+import cn.ucai.superwechat.bean.UserAvatar;
+import cn.ucai.superwechat.utils.OkHttpUtils2;
+import cn.ucai.superwechat.utils.Utils;
+
+/**
+ * Created by Administrator on 2016/7/26.
+ */
+public class DownloadGroupListTask {
+    String username;
+    private static final String TAG = DownloadContactListTask.class.getSimpleName();
+    Context mContext;
+
+    public DownloadGroupListTask(String username,Context context) {
+        this.mContext = context;
+        this.username = username;
+    }
+
+    public void execute() {
+        final OkHttpUtils2<String> utils2 = new OkHttpUtils2<>();
+        utils2.setRequestUrl(I.REQUEST_FIND_GROUP_BY_USER_NAME)
+                .addParam(I.Contact.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(new OkHttpUtils2.OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Result fromJson = Utils.getListResultFromJson(result, UserAvatar.class);
+                        Log.e(TAG, "result=" + fromJson);
+                        List<GroupAvatar> list = (List<GroupAvatar>) fromJson.getRetData();
+                        if (list != null && list.size() > 0) {
+                            Log.e(TAG, "size=" + list.size());
+                            SuperWeChatApplication.getInstance().setGrouplist (list);
+                            mContext.sendStickyBroadcast(new Intent("update_group_list"));
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e(TAG, "error="+error);
+
+                    }
+                });
+    }
+}
