8da47648673eb141f26df3d0a1eb08defd5fc02b
BlackFox <3296895445@qq.com>
2016-08-01 17:31:05 +0800

    FuLiCenterMainActivity及其布局文件的创建

---
+++ src/cn/ucai/fulicenter/activity/FuLiCenterMainActivity.java
@@ -0,0 +1,19 @@
+package cn.ucai.fulicenter.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+public class FuLiCenterMainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_fulicenter);
+    }
+
+}
