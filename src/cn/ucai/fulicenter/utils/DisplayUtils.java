df623c08d4bb5ad4ea7bc7c896dfe8c11dd3739d
BlackFox <3296895445@qq.com>
2016-08-09 15:15:46 +0800

    DisplayUtils中添加用来设置标题并结束当前Activity的方法

--- src/cn/ucai/fulicenter/utils/DisplayUtils.java
+++ src/cn/ucai/fulicenter/utils/DisplayUtils.java
@@ -2,6 +2,7 @@ package cn.ucai.fulicenter.utils;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.TextView;
 
 import cn.ucai.fulicenter.R;
 
@@ -17,6 +18,19 @@ public class DisplayUtils {
 
             }
         });
+
+    }
+    public static void initBackWithTitle(final Activity activity,String title) {
+
+        ((TextView)activity.findViewById(R.id.tv_back)).setText(title);
+        activity.findViewById(R.id.iv_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.finish();
+
+            }
+        });
+
 
     }
 }
