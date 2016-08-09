5b57f32339649f4ba47d381c8135a4d76eac254b
BlackFox <3296895445@qq.com>
2016-08-09 16:17:32 +0800

    分享到第三方应用平台相关文件的导入以及分享按钮单击事件的具体实现

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -9,6 +9,9 @@ import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulicenter.D;
 import cn.ucai.fulicenter.DemoHXSDKHelper;
 import cn.ucai.fulicenter.FuLiCenterApplication;
@@ -49,89 +52,123 @@ public class GoodDetailsActivity extends BaseActivity {
     private void setListener() {
         CollectListener mListener=new CollectListener();
         ivCollect.setOnClickListener(mListener);
+        ivShared.setOnClickListener(mListener);
 
 
     }
+
 
     class CollectListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
-            if (DemoHXSDKHelper.getInstance().isLogined()) {
+
 
                 switch (v.getId()) {
                     case R.id.iv_collect:
-                        if (isCollected) {
-                            OkHttpUtils2<MessageBean> utils2 = new OkHttpUtils2<MessageBean>();
-                            utils2.setRequestUrl(I.REQUEST_DELETE_COLLECT)
-                                    .addParam(I.Collect.USER_NAME, FuLiCenterApplication.getInstance().getUserName())
-                                    .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodId))
-                                    .targetClass(MessageBean.class)
-                                    .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
-                                        @Override
-                                        public void onSuccess(MessageBean msg) {
-                                            Log.e(TAG, "result=" + msg);
-                                            if (msg != null && msg.isSuccess()) {
-                                                new DownloadCollectCountTask(FuLiCenterApplication.getInstance().getUserName(), mContext).execute();
-                                                isCollected = false;
-                                                setCollectButton();
-                                                sendStickyBroadcast(new Intent("update_collect_list"));
+                        if (DemoHXSDKHelper.getInstance().isLogined()) {
+                            if (isCollected) {
+                                OkHttpUtils2<MessageBean> utils2 = new OkHttpUtils2<MessageBean>();
+                                utils2.setRequestUrl(I.REQUEST_DELETE_COLLECT)
+                                        .addParam(I.Collect.USER_NAME, FuLiCenterApplication.getInstance().getUserName())
+                                        .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodId))
+                                        .targetClass(MessageBean.class)
+                                        .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                                            @Override
+                                            public void onSuccess(MessageBean msg) {
+                                                Log.e(TAG, "result=" + msg);
+                                                if (msg != null && msg.isSuccess()) {
+                                                    new DownloadCollectCountTask(FuLiCenterApplication.getInstance().getUserName(), mContext).execute();
+                                                    isCollected = false;
+                                                    setCollectButton();
+                                                    sendStickyBroadcast(new Intent("update_collect_list"));
 
-                                            } else {
-                                                Log.e(TAG, "delete collect id failed");
+                                                } else {
+                                                    Log.e(TAG, "delete collect id failed");
+
+                                                }
+                                                Toast.makeText(mContext, msg.getMsg(), Toast.LENGTH_SHORT).show();
 
                                             }
-                                            Toast.makeText(mContext, msg.getMsg(), Toast.LENGTH_SHORT).show();
 
-                                        }
-
-                                        @Override
-                                        public void onError(String error) {
-                                            Log.e(TAG, "error=" + error);
+                                            @Override
+                                            public void onError(String error) {
+                                                Log.e(TAG, "error=" + error);
 
 
-                                        }
-                                    });
+                                            }
+                                        });
 
+                            } else {
+                                OkHttpUtils2<MessageBean> utils = new OkHttpUtils2<>();
+                                utils.setRequestUrl(I.REQUEST_ADD_COLLECT)
+                                        .addParam(I.Collect.USER_NAME,FuLiCenterApplication.getInstance().getUserName())
+                                        .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodDetails.getGoodsId()))
+                                        .addParam(I.Collect.ADD_TIME, String.valueOf(mGoodDetails.getAddTime()))
+                                        .addParam(I.Collect.GOODS_ENGLISH_NAME,mGoodDetails.getGoodsEnglishName())
+                                        .addParam(I.Collect.GOODS_THUMB,mGoodDetails.getGoodsThumb())
+                                        .addParam(I.Collect.GOODS_IMG,mGoodDetails.getGoodsImg())
+                                        .addParam(I.Collect.GOODS_NAME,mGoodDetails.getGoodsName())
+                                        .targetClass(MessageBean.class)
+                                        .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                                            @Override
+                                            public void onSuccess(MessageBean result) {
+                                                Log.e(TAG, "result=" + result);
+                                                if (result != null && result.isSuccess()) {
+                                                    isCollected = true;
+                                                    setCollectButton();
+                                                    new DownloadCollectCountTask(FuLiCenterApplication.getInstance().getUserName(), mContext).execute();
+
+                                                }
+
+                                            }
+
+                                            @Override
+                                            public void onError(String error) {
+                                                Log.e(TAG, "error=" + error);
+
+                                            }
+                                        });
+                            }
                         } else {
-                            OkHttpUtils2<MessageBean> utils = new OkHttpUtils2<>();
-                            utils.setRequestUrl(I.REQUEST_ADD_COLLECT)
-                                    .addParam(I.Collect.USER_NAME,FuLiCenterApplication.getInstance().getUserName())
-                                    .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodDetails.getGoodsId()))
-                                    .addParam(I.Collect.ADD_TIME, String.valueOf(mGoodDetails.getAddTime()))
-                                    .addParam(I.Collect.GOODS_ENGLISH_NAME,mGoodDetails.getGoodsEnglishName())
-                                    .addParam(I.Collect.GOODS_THUMB,mGoodDetails.getGoodsThumb())
-                                    .addParam(I.Collect.GOODS_IMG,mGoodDetails.getGoodsImg())
-                                    .addParam(I.Collect.GOODS_NAME,mGoodDetails.getGoodsName())
-                                    .targetClass(MessageBean.class)
-                                    .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
-                                        @Override
-                                        public void onSuccess(MessageBean result) {
-                                            Log.e(TAG, "result=" + result);
-                                            if (result != null && result.isSuccess()) {
-                                                isCollected = true;
-                                                setCollectButton();
-                                                new DownloadCollectCountTask(FuLiCenterApplication.getInstance().getUserName(), mContext).execute();
-
-                                            }
-
-                                        }
-
-                                        @Override
-                                        public void onError(String error) {
-                                            Log.e(TAG, "error=" + error);
-
-                                        }
-                                    });
-
+                            startActivity(new Intent(GoodDetailsActivity.this,LoginActivity.class));
                         }
 
+                            break;
+                    case R.id.iv_shared:
+                        showShare();
                         break;
+
                 }
-            } else {
-                startActivity(new Intent(GoodDetailsActivity.this,LoginActivity.class));
-            }
         }
+    }
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl(mGoodDetails.getShareUrl());
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(mGoodDetails.getGoodsName());
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl(mGoodDetails.getShareUrl());
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment(mGoodDetails.getGoodsName());
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl(mGoodDetails.getShareUrl());
+
+        // 启动分享GUI
+        oks.show(this);
     }
 
     private void setCollectButton() {
