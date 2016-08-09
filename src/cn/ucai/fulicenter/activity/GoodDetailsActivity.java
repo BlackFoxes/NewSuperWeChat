de4989b645d9c69c9558ce032a8a106df27874d7
BlackFox <3296895445@qq.com>
2016-08-09 15:38:23 +0800

    商品详情界面收藏按钮的单击事件（实现添加收藏，改变按钮颜色以及删除收藏的功能）

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -1,17 +1,23 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import cn.ucai.fulicenter.D;
+import cn.ucai.fulicenter.DemoHXSDKHelper;
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.AlbumsBean;
 import cn.ucai.fulicenter.bean.GoodDetailsBean;
+import cn.ucai.fulicenter.bean.MessageBean;
+import cn.ucai.fulicenter.task.DownloadCollectCountTask;
 import cn.ucai.fulicenter.utils.DisplayUtils;
 import cn.ucai.fulicenter.utils.OkHttpUtils2;
 import cn.ucai.fulicenter.view.FlowIndicator;
@@ -28,6 +34,7 @@ public class GoodDetailsActivity extends BaseActivity {
     FlowIndicator mFlowIndicator;
     static final String TAG = GoodDetailsActivity.class.getSimpleName();
     int mGoodId;
+    boolean isCollected;
     GoodDetailsBean mGoodDetails;
     @Override
     protected void onCreate(Bundle arg0) {
@@ -36,7 +43,109 @@ public class GoodDetailsActivity extends BaseActivity {
         mContext = this;
         initView();
         initData();
+        setListener();
     }
+
+    private void setListener() {
+        CollectListener mListener=new CollectListener();
+        ivCollect.setOnClickListener(mListener);
+
+
+    }
+
+    class CollectListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (DemoHXSDKHelper.getInstance().isLogined()) {
+
+                switch (v.getId()) {
+                    case R.id.iv_collect:
+                        if (isCollected) {
+                            OkHttpUtils2<MessageBean> utils2 = new OkHttpUtils2<MessageBean>();
+                            utils2.setRequestUrl(I.REQUEST_DELETE_COLLECT)
+                                    .addParam(I.Collect.USER_NAME, FuLiCenterApplication.getInstance().getUserName())
+                                    .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodId))
+                                    .targetClass(MessageBean.class)
+                                    .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                                        @Override
+                                        public void onSuccess(MessageBean msg) {
+                                            Log.e(TAG, "result=" + msg);
+                                            if (msg != null && msg.isSuccess()) {
+                                                new DownloadCollectCountTask(FuLiCenterApplication.getInstance().getUserName(), mContext).execute();
+                                                isCollected = false;
+                                                setCollectButton();
+                                                sendStickyBroadcast(new Intent("update_collect_list"));
+
+                                            } else {
+                                                Log.e(TAG, "delete collect id failed");
+
+                                            }
+                                            Toast.makeText(mContext, msg.getMsg(), Toast.LENGTH_SHORT).show();
+
+                                        }
+
+                                        @Override
+                                        public void onError(String error) {
+                                            Log.e(TAG, "error=" + error);
+
+
+                                        }
+                                    });
+
+                        } else {
+                            OkHttpUtils2<MessageBean> utils = new OkHttpUtils2<>();
+                            utils.setRequestUrl(I.REQUEST_ADD_COLLECT)
+                                    .addParam(I.Collect.USER_NAME,FuLiCenterApplication.getInstance().getUserName())
+                                    .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodDetails.getGoodsId()))
+                                    .addParam(I.Collect.ADD_TIME, String.valueOf(mGoodDetails.getAddTime()))
+                                    .addParam(I.Collect.GOODS_ENGLISH_NAME,mGoodDetails.getGoodsEnglishName())
+                                    .addParam(I.Collect.GOODS_THUMB,mGoodDetails.getGoodsThumb())
+                                    .addParam(I.Collect.GOODS_IMG,mGoodDetails.getGoodsImg())
+                                    .addParam(I.Collect.GOODS_NAME,mGoodDetails.getGoodsName())
+                                    .targetClass(MessageBean.class)
+                                    .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                                        @Override
+                                        public void onSuccess(MessageBean result) {
+                                            Log.e(TAG, "result=" + result);
+                                            if (result != null && result.isSuccess()) {
+                                                isCollected = true;
+                                                setCollectButton();
+                                                new DownloadCollectCountTask(FuLiCenterApplication.getInstance().getUserName(), mContext).execute();
+
+                                            }
+
+                                        }
+
+                                        @Override
+                                        public void onError(String error) {
+                                            Log.e(TAG, "error=" + error);
+
+                                        }
+                                    });
+
+                        }
+
+                        break;
+                }
+            } else {
+                startActivity(new Intent(GoodDetailsActivity.this,LoginActivity.class));
+            }
+        }
+    }
+
+    private void setCollectButton() {
+        if (isCollected) {
+            ivCollect.setImageResource(R.drawable.bg_collect_out);
+
+
+        } else {
+            ivCollect.setImageResource(R.drawable.bg_collect_in);
+
+        }
+
+    }
+
     private void initData() {
         mGoodId = getIntent().getIntExtra(D.GoodDetails.KEY_GOODS_ID, 0);
         Log.e(TAG, "mGoodId=" + mGoodId);
@@ -112,5 +221,45 @@ public class GoodDetailsActivity extends BaseActivity {
         WebSettings settings = mWebView.getSettings();
         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         settings.setBuiltInZoomControls(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (DemoHXSDKHelper.getInstance().isLogined()) {
+            initCollectImage();
+        }
+    }
+
+    private void initCollectImage() {
+        OkHttpUtils2<MessageBean> utils2 = new OkHttpUtils2<>();
+        utils2.setRequestUrl(I.REQUEST_IS_COLLECT)
+                .addParam(I.Collect.USER_NAME, FuLiCenterApplication.getInstance().getUserName())
+                .addParam(I.Collect.GOODS_ID, String.valueOf(mGoodId))
+                .targetClass(MessageBean.class)
+                .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                    @Override
+                    public void onSuccess(MessageBean msg) {
+                        Log.e(TAG, "result=" + msg);
+                        if (msg != null && msg.isSuccess()) {
+                            ivCollect.setImageResource(R.drawable.bg_collect_out);
+                            isCollected = true;
+                        } else {
+                            Log.e(TAG, "REQUEST_IS_COLLECT is failed");
+                            isCollected = false;
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e(TAG, "error=" + error);
+
+                    }
+                });
+
+
+
     }
 }
