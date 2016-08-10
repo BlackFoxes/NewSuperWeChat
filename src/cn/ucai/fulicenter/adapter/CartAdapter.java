e23857722b0499e072ea200288c38d630e9fb41d
BlackFox <3296895445@qq.com>
2016-08-10 22:44:50 +0800

    CartAdapter中CheckBox单击事件的监听以及商品数据的正确显示

--- src/cn/ucai/fulicenter/adapter/CartAdapter.java
+++ src/cn/ucai/fulicenter/adapter/CartAdapter.java
@@ -8,38 +8,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.activity.BoutiqueDetailsActivity;
 import cn.ucai.fulicenter.activity.FootHolder;
 import cn.ucai.fulicenter.bean.BoutiqueBean;
 import cn.ucai.fulicenter.bean.CartBean;
+import cn.ucai.fulicenter.bean.MessageBean;
+import cn.ucai.fulicenter.task.UpdateCartTask;
 import cn.ucai.fulicenter.utils.ImageUtils;
+import cn.ucai.fulicenter.utils.OkHttpUtils2;
 
 /**
  * Created by Administrator on 2016/8/3.
  */
 public class CartAdapter extends RecyclerView.Adapter {
-    ArrayList<CartBean> mArrayList;
+    ArrayList<CartBean> mlist;
     Context mContext;
     RecyclerView.ViewHolder holder;
     static final String TAG = CartAdapter.class.getSimpleName();
     public CartAdapter(Context context, ArrayList<CartBean> boutiqueArrayList) {
+        mlist = new ArrayList<>();
         this.mContext = context;
-        this.mArrayList = boutiqueArrayList;
+        mlist.addAll(boutiqueArrayList);
     }
-
-
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Log.e(TAG, "BoutiqueAdapter.onCreateViewHolder.isChecked");
-        View itemBoutique = LayoutInflater.from(mContext).inflate(R.layout.item_boutique, null, false);
-        holder = new CartItemHolder(itemBoutique);
+        View itemCart = LayoutInflater.from(mContext).inflate(R.layout.item_cart, null, false);
+        holder = new CartItemHolder(itemCart);
         return holder;
     }
     @Override
@@ -47,49 +53,122 @@ public class CartAdapter extends RecyclerView.Adapter {
         Log.e(TAG, "BoutiqueAdapter.onBindViewHolder.isChecked");
         Log.e(TAG, "BoutiqueAdapter.onBindViewHolder.position="+position);
 
-           final CartBean cartBean = mArrayList.get(position);
+           final CartBean cartBean = mlist.get(position);
             String currencyPrice = cartBean.getGoods().getCurrencyPrice();
             int price = Integer.parseInt(currencyPrice.substring(currencyPrice.indexOf("￥") + 1));
             int allPrice=(cartBean.getCount())*price;
-            ((CartItemHolder)holder).tvCartTitle.setText("￥"+allPrice);
+            ((CartItemHolder)holder).tvCartTitle.setText(cartBean.getGoods().getGoodsName());
                 ((CartItemHolder)holder).tvCartCount.setText("("+cartBean.getCount()+")");
+                ((CartItemHolder)holder).tvCartPrice.setText("￥"+allPrice);
+                ((CartItemHolder)holder).mCheckBox.setChecked(cartBean.isChecked());
                 ImageUtils.setGoodAvatar(cartBean.getGoods().getGoodsThumb(),mContext,((CartItemHolder)holder).mCartAvatar);
-                ((CartItemHolder)holder).mCartDelete.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(mContext, BoutiqueDetailsActivity.class);
-                        intent.putExtra(I.NewAndBoutiqueGood.CAT_ID, cartBean.getId());
-                        mContext.startActivity(intent);
-                    }
-                });
-            ((CartItemHolder)holder).mCartAdd.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(mContext, BoutiqueDetailsActivity.class);
-                    intent.putExtra(I.NewAndBoutiqueGood.CAT_ID, cartBean.getId());
-                    mContext.startActivity(intent);
-                }
-            });
+                ((CartItemHolder)holder).mCartDelete.setOnClickListener(new CartChangedListener(cartBean,-1));
+            ((CartItemHolder)holder).mCartAdd.setOnClickListener(new CartChangedListener(cartBean,1));
+        ((CartItemHolder)holder).mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cartBean.setChecked(isChecked);
+                new UpdateCartTask(mContext, cartBean).execute();
 
 
 
-
+            }
+        });
     }
 
+    class CartChangedListener implements View.OnClickListener {
+        CartBean mCart;
+        int num;
+
+        public CartChangedListener(CartBean mCart, int count) {
+            this.mCart = mCart;
+            this.num = count;
+        }
+//        url=http://10.0.2.2:8080/ FuLiCenterServer/Server?request=add_cart&goodsId=&userName=
+//                & goodsName=&goodsEnglishName=&goodsThumb=&goodsImg=&addTime=&isSelected=
+
+        @Override
+        public void onClick(View v) {
+            mCart.setCount(mCart.getCount() + num);
+            if (num > 0) {
+                OkHttpUtils2<MessageBean> utils2 = new OkHttpUtils2<>();
+                try {
+                    utils2.setRequestUrl(I.REQUEST_ADD_CART)
+                            .addParam(I.Cart.GOODS_ID, String.valueOf(mCart.getGoodsId()))
+                            .addParam(I.Cart.USER_NAME, mCart.getUserName())
+                            .addParam(I.Collect.GOODS_NAME, URLEncoder.encode(mCart.getGoods().getGoodsName(),"utf-8"))
+                            .addParam(I.Collect.GOODS_ENGLISH_NAME, mCart.getGoods().getGoodsEnglishName())
+                            .addParam(I.Cart.GOODS_THUMB, mCart.getGoods().getGoodsThumb())
+                            .addParam(I.Collect.GOODS_IMG, mCart.getGoods().getGoodsImg())
+                            .addParam(I.Collect.ADD_TIME, String.valueOf(mCart.getGoods().getAddTime()))
+                            .targetClass(MessageBean.class)
+                            .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                                @Override
+                                public void onSuccess(MessageBean result) {
+                                    if (result != null && result.isSuccess()) {
+                                        Log.e(TAG, "result=" + result);
+                                        ArrayList<CartBean> cartList = FuLiCenterApplication.getInstance().getCartList();
+                                        if (cartList.indexOf(mCart)>0) {
+                                            cartList.get(cartList.indexOf(mCart)).setCount((mCart.getCount() + num));
+                                            mContext.sendStickyBroadcast(new Intent("update_cart_list"));
+
+                                    }
+                                    }
+
+                                }
+
+                                @Override
+                                public void onError(String error) {
+                                    Log.e(TAG, "error=" + error);
+
+
+                                }
+                            });
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                OkHttpUtils2<MessageBean> utils = new OkHttpUtils2<>();
+                utils.setRequestUrl(I.REQUEST_DELETE_CART)
+                        .addParam(I.Cart.ID, String.valueOf(mCart.getId()))
+                        .targetClass(MessageBean.class)
+                        .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                            @Override
+                            public void onSuccess(MessageBean result) {
+                                Log.e(TAG, "result=" + result);
+                                ArrayList<CartBean> cartList = FuLiCenterApplication.getInstance().getCartList();
+                                if (cartList.indexOf(mCart)>0) {
+                                    cartList.get(cartList.indexOf(mCart)).setCount((mCart.getCount() + num));
+                                    mContext.sendStickyBroadcast(new Intent("update_cart_list"));
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e(TAG, "error=" + error);
+                            }
+                        });
+
+            }
+
+        }
+    }
     @Override
     public int getItemCount() {
-        return mArrayList.size()!=0?mArrayList.size():0;
+        return mlist.size()!=0?mlist.size():0;
     }
-
     public void initBoutiqueData(ArrayList<CartBean> list) {
-        if (mArrayList != null) {
-
-            mArrayList.clear();
+        Log.e(TAG, "getItemCount,list=" + list);
+        Log.e(TAG, "getItemCount,mArrayList=" + mlist);
+        if (mlist != null) {
+            mlist.clear();
         }
-        mArrayList.addAll(list);
+        mlist.addAll(list);
+        Log.e(TAG, "getItemCount,list1=" + list);
+        Log.e(TAG, "getItemCount,mArrayList1=" + mlist);
         notifyDataSetChanged();
     }
-
     class CartItemHolder extends RecyclerView.ViewHolder {
         CheckBox mCheckBox;
         ImageView mCartAvatar,mCartDelete,mCartAdd;
