a1bfb8eb57f6ff1e1616a913a65c0488bcf377e5
BlackFox <3296895445@qq.com>
2016-08-11 13:37:20 +0800

    UpdateCartTask的修改

--- src/cn/ucai/fulicenter/task/UpdateCartTask.java
+++ src/cn/ucai/fulicenter/task/UpdateCartTask.java
@@ -26,37 +26,99 @@ public class UpdateCartTask {
     }
 
     public void execute() {
+
         final OkHttpUtils2<MessageBean> utils2 = new OkHttpUtils2<>();
-        utils2.setRequestUrl(I.REQUEST_UPDATE_CART)
-                .addParam(I.Cart.ID, String.valueOf(mCartBean.getId()))
-                .addParam(I.Cart.COUNT, String.valueOf(mCartBean.getCount()))
-                .addParam(I.Cart.IS_CHECKED, String.valueOf(mCartBean.isChecked()))
-                .targetClass(MessageBean.class)
-                .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
-                    @Override
-                    public void onSuccess(MessageBean result) {
-                        if (result != null && result.isSuccess()) {
-                            ArrayList<CartBean> cartList = FuLiCenterApplication.getInstance().getCartList();
-                            if (cartList.indexOf(mCartBean)>0) {
-                                cartList.get(cartList.indexOf(mCartBean)).setChecked(mCartBean.isChecked());
-                                cartList.get(cartList.indexOf(mCartBean)).setCount(mCartBean.getCount());
+
+        if (mCartBean.getId() == 0) {
+            utils2.setRequestUrl(I.REQUEST_ADD_CART)
+                    .addParam(I.Cart.GOODS_ID, String.valueOf(mCartBean.getGoodsId()))
+                    .addParam(I.Cart.USER_NAME, mCartBean.getUserName())
+                    .addParam(I.Cart.IS_CHECKED, String.valueOf(mCartBean.isChecked()))
+                    .addParam(I.Cart.COUNT, String.valueOf(mCartBean.getCount()))
+                    .targetClass(MessageBean.class)
+                    .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                        @Override
+                        public void onSuccess(MessageBean result) {
+                            if (result != null && result.isSuccess()) {
+                                Log.e(TAG, "mCartBean.getId() == 0,result=" + result);
+                                new DownloadCartListTask(FuLiCenterApplication.getInstance().getUserName(),
+                                        mContext).execute();
+                            }
+
+                        }
+                        @Override
+                        public void onError(String error) {
+                            Log.e(TAG, "error=" + error);
+                        }
+                    });
+        } else {
+            if (mCartBean.getCount() > 0) {
+                utils2.setRequestUrl(I.REQUEST_UPDATE_CART)
+                        .addParam(I.Cart.ID, String.valueOf(mCartBean.getId()))
+                        .addParam(I.Cart.COUNT, String.valueOf(mCartBean.getCount()))
+                        .addParam(I.Cart.IS_CHECKED, String.valueOf(mCartBean.isChecked()))
+                        .targetClass(MessageBean.class)
+                        .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                            @Override
+                            public void onSuccess(MessageBean result) {
+                                if (result != null && result.isSuccess()) {
+                                    ArrayList<CartBean> cartList = FuLiCenterApplication.getInstance().getCartList();
+                                    if (cartList.indexOf(mCartBean)>0) {
+                                        cartList.get(cartList.indexOf(mCartBean)).setChecked(mCartBean.isChecked());
+                                        cartList.get(cartList.indexOf(mCartBean)).setCount(mCartBean.getCount());
 //                                cartList.set(cartList.indexOf(mCartBean), mCartBean);
 
 
+                                    }
+                                    Log.e(TAG, "result=" + result);
+                                }
+                                mContext.sendStickyBroadcast(new Intent("update_cart_list"));
+
                             }
-                            Log.e(TAG, "result=" + result);
-                        }
-                        mContext.sendStickyBroadcast(new Intent("update_cart_list"));
 
-                    }
-
-                    @Override
-                    public void onError(String error) {
-                        Log.e(TAG, "error=" + error);
+                            @Override
+                            public void onError(String error) {
+                                Log.e(TAG, "error=" + error);
 
 
-                    }
-                });
+                            }
+                        });
+            } else if (mCartBean.getCount() <= 0) {
+                utils2.setRequestUrl(I.REQUEST_DELETE_CART)
+                        .addParam(I.Cart.ID, String.valueOf(mCartBean.getId()))
+                        .targetClass(MessageBean.class)
+                        .execute(new OkHttpUtils2.OnCompleteListener<MessageBean>() {
+                            @Override
+                            public void onSuccess(MessageBean result) {
+                                if (result != null && result.isSuccess()) {
+                                    ArrayList<CartBean> cartList = FuLiCenterApplication.getInstance().getCartList();
+                                    if (cartList.indexOf(mCartBean)>0) {
+                                        cartList.remove(cartList.indexOf(mCartBean));
+//                                cartList.set(cartList.indexOf(mCartBean), mCartBean);
+
+
+                                    }
+                                    Log.e(TAG, "result=" + result);
+                                }
+                                mContext.sendStickyBroadcast(new Intent("update_cart_list"));
+
+
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e(TAG, "error=" + error);
+
+
+                            }
+                        });
+//            url=http://10.0.2.2:8080/ FuLiCenterServer/Server?request=add_cart&goodsId=&userName=
+//            & goodsName=&goodsEnglishName=&goodsThumb=&goodsImg=&addTime=&isSelected=
+
+
+            }
+
+        }
 
 
 
